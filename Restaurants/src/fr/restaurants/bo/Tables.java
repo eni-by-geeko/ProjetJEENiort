9be090f31739327315bbddb14150e4cf6369152d
Kchriss <52273624+Kchriss@users.noreply.github.com>
2019-07-30 15:26:26 +0200

    client "lourd"

--- Restaurants/src/fr/restaurants/bo/Tables.java
+++ Restaurants/src/fr/restaurants/bo/Tables.java
@@ -1,5 +1,29 @@
 package fr.restaurants.bo;
 
 public class Tables {
+
 private int numero;
+private String statut;
+
+public Tables() {
+		// TODO Auto-generated constructor stub
+	}
+public Tables(int numero, String statut) {
+	super();
+	this.numero = numero;
+	this.statut = statut;
+}
+public int getNumero() {
+	return numero;
+}
+public void setNumero(int numero) {
+	this.numero = numero;
+}
+public String getStatut() {
+	return statut;
+}
+public void setStatut(String statut) {
+	this.statut = statut;
+}
+
 }
